dd0a297a0810cc21ee3b352d78b1c9d0d365fd36
tie211068 <tie211068@S1705C10438-T1.tisnt.tis.co.jp>
2018-06-21 09:25:42 +0900

    a

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,10 +1,20 @@
 package codecheck;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class App {
 	public static void main(String[] args) {
+
+        Map<String,String> in_map = new HashMap<String,String>();
+
 		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
+
+			String[] data = args[i].split(":");
+			in_map.put(data[0], data[1]);
 		}
+        System.out.println(in_map);
 	}
+
 }
