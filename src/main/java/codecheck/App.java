ee1f211d19f1acf7e1777ef7fbcbb6373dec3a1c
tie211068 <tie211068@S1705C10438-T1.tisnt.tis.co.jp>
2018-06-21 09:30:15 +0900

    a

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -8,13 +8,15 @@ public class App {
 	public static void main(String[] args) {
 
         Map<String,String> in_map = new HashMap<String,String>();
+        int last = Integer.parseInt(args[args.length]);
 
-		for (int i = 0, l = args.length; i < l; i++) {
+		for (int i = 0, l = args.length-1; i < l; i++) {
 
 			String[] data = args[i].split(":");
 			in_map.put(data[0], data[1]);
 		}
-        System.out.println(in_map);
+        System.out.println(in_map + String.valueOf(last));
+
 	}
 
 }
